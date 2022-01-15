1914e4ab0d9540c487426e1818828723e568a2e2
shinbunbun <takamuneyuto@gmail.com>
2022-01-15 15:16:13 +0000

    delete comment

--- src/hmac.c
+++ src/hmac.c
@@ -11,35 +11,17 @@ int k64_encode(const char *hmac, int hmaclen, char *const encoded)
 
   BIO *mem = BIO_new(BIO_s_mem());
   printf("%s\n", hmac);
-  // int buf_len = BIO_puts(mem, hmac);
   BIO *b64 = BIO_new(BIO_f_base64());
   BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-  // BIO_set_mem_eof_return(b64, BIO_NOCLOSE);
   BIO_push(b64, mem);
   BIO_write(b64, hmac, hmaclen);
   BIO_flush(b64);
 
   BIO_seek(mem, 0);
-  // BIO_write(mem, "\n", 1);
   int buf_len = BIO_read(mem, encoded, 300);
   encoded[buf_len] = '\0';
   printf("%s\n", encoded);
   BIO_free_all(mem);
-
-  /* BIO *bio, *b64;
-
-  b64 = BIO_new(BIO_f_base64());
-  FILE *output;
-  bio = BIO_new_fp(stdout, BIO_NOCLOSE);
-  BIO_push(b64, bio);
-  BIO_write(b64, data, strlen(data));
-  printf("0\n");
-  BIO_read(bio, buf, buf_size);
-  printf("1\n");
-  printf("%s\n", buf);
-  BIO_flush(b64);
-
-  BIO_free_all(b64); */
 }
 
 void create_hmac(char *data, char *buf, size_t buf_size)
